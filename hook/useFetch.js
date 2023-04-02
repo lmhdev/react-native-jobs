21068434b83d33ac8ff7c60c6169db7bdb79ce94
Le Minh Hoang <hoang.lm@urbox.vn>
2023-04-02 22:43:16 +0700

    Init

---
+++ hook/useFetch.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  REACT_APP_X_RAPID_API_URL,
+  REACT_APP_X_RAPID_API_KEY,
+  REACT_APP_X_RAPID_API_HOST,
+} from "@env";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `${REACT_APP_X_RAPID_API_URL}/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": REACT_APP_X_RAPID_API_KEY,
+      "X-RapidAPI-Host": REACT_APP_X_RAPID_API_HOST,
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
